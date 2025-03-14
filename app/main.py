de464ff103c4e8d66e64172866b5e5d2a7bacc60
roman <skyfoxwork@gmail.com>
2025-03-14 19:36:49 +0200

    refactor app/main.py module with config settings

--- app/main.py
+++ app/main.py
@@ -6,20 +6,20 @@ import psycopg2
 
 from parser import parse_save_products
 from database import create_database, print_total_number_of_data
-from settings import URLS_TO_SCRAPE, DB_CONFIG, TABLE_NAME
+from config import settings
 
 
 def main() -> None:
-    with psycopg2.connect(**DB_CONFIG) as conn:
+    with psycopg2.connect(**settings.DB_CONFIG) as conn:
         create_database(conn)
         print(
             f"Database: ({os.getenv('POSTGRES_DB_NAME')}) "
-            f"is created with table name: ({TABLE_NAME})",
+            f"is created with table name: ({settings.TABLE_NAME})",
             "\n"
         )
 
         with httpx.Client(timeout=60) as client:
-            for name, url in URLS_TO_SCRAPE.items():
+            for name, url in settings.URLS_TO_SCRAPE.items():
                 print(">>>", name, url, "\n")
                 parse_save_products(client, url, conn)
 
@@ -28,5 +28,8 @@ if __name__ == "__main__":
     start = time.time()
     main()
     print("-" * 50)
-    print("total number of products:", print_total_number_of_data(TABLE_NAME))
+    print(
+        "total number of products:",
+        print_total_number_of_data(settings.TABLE_NAME)
+    )
     print("run time:", time.time() - start)
