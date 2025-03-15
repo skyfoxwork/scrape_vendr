ead4c00f7a42333a64a835e0fde99d77ea2431cd
roman <skyfoxwork@gmail.com>
2025-03-15 16:13:27 +0200

    add app_logging.py module with logging settings

---
+++ app/app_logging.py
@@ -0,0 +1,25 @@
+import logging
+from config import settings
+
+# create logger
+logger = logging.getLogger(settings.PROJECT_NAME)
+
+if settings.LOGGING_DEBUG == True:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
+# format
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+# create console handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+# create file handler
+file_handler = logging.FileHandler(settings.LOGGING_FILE_NAME, encoding="utf-8")
+file_handler.setFormatter(formatter)
+
+# add handlers to logger
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
