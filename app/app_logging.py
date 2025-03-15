d10792c34c4715d4f86eeae0a50abfeba4d290cf
roman <skyfoxwork@gmail.com>
2025-03-15 18:12:25 +0200

    fix app/app_logging.py

--- app/app_logging.py
+++ app/app_logging.py
@@ -1,10 +1,11 @@
 import logging
 from config import settings
 
+
 # create logger
 logger = logging.getLogger(settings.PROJECT_NAME)
 
-if settings.LOGGING_DEBUG is True:
+if settings.LOGGING_DEBUG:
     logger.setLevel(logging.DEBUG)
 else:
     logger.setLevel(logging.INFO)
@@ -16,12 +17,14 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
 
-# create file handler
-file_handler = logging.FileHandler(
-    settings.LOGGING_FILE_NAME, encoding="utf-8"
-)
-file_handler.setFormatter(formatter)
-
 # add handlers to logger
 logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+
+if settings.LOGGING_WRITE_TO_FILE:
+    # create file handler
+    file_handler = logging.FileHandler(
+        settings.LOGGING_FILE_NAME, encoding="utf-8"
+    )
+    file_handler.setFormatter(formatter)
+    # add handlers to logger
+    logger.addHandler(file_handler)
